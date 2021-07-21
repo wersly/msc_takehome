c6c64699d048caa42dfa94ae9757035a1df8c88f
warren <warren.b.ersly@gmail.com>
2021-07-20 23:43:18 -0400

    Refactor reports into config.py; reorder fns in routes.py

--- msc_takehome/config.py
+++ msc_takehome/config.py
@@ -1,2 +1,21 @@
 class Config(object):
+
     DB_FILE = "orchestra.db"
+    REPORTS = [
+        {
+            "view": "all_musicians",
+            "name": "Name, instrument, and section for all musicians"
+        },
+        {
+            "view": "instruments_without_musicians",
+            "name": "Instruments that don't yet have musicians (i.e. no one plays the trumpet), and their sections"
+        },
+        {
+            "view": "multi_instrumentalists",
+            "name": "Any musicians that play two or more instruments, their instrument, and section"
+        },
+        {
+            "view": "multiple_players",
+            "name": "Any instruments that are played by multiple musicians, as well as the musician names and sections"
+        }
+    ]
--- msc_takehome/routes.py
+++ msc_takehome/routes.py
@@ -6,25 +6,6 @@ import sqlite3
 app = Flask(__name__)
 app.config.from_object("config.Config")
 
-reports = [
-    {
-        "view": "all_musicians",
-        "name": "Name, instrument, and section for all musicians"
-    },
-    {
-        "view": "instruments_without_musicians",
-        "name": "Instruments that don't yet have musicians (i.e. no one plays the trumpet), and their sections"
-    },
-    {
-        "view": "multi_instrumentalists",
-        "name": "Any musicians that play two or more instruments, their instrument, and section"
-    },
-    {
-        "view": "multiple_players",
-        "name": "Any instruments that are played by multiple musicians, as well as the musician names and sections"
-    }
-]
-
 
 def get_db(db_file):
 
@@ -50,12 +31,6 @@ def process_report_df(df):
     return df
 
 
-@app.route("/")
-def index():
-
-    return render_template("index.html", reports=reports)
-
-
 @app.teardown_appcontext
 def close_connection(exception):
 
@@ -67,13 +42,19 @@ def close_connection(exception):
     return None
 
 
+@app.route("/")
+def index():
+
+    return render_template("index.html", reports=app.config.get("REPORTS"))
+
+
 @app.route("/report/<view_name>")
 def report(view_name):
 
     conn = get_db(app.config.get("DB_FILE"))
     query = "select * from {view_name}".format(view_name=view_name)
     df = process_report_df(pd.read_sql(query, conn))
-    name = list(filter(lambda x: x["view"] == view_name, reports))[0]["name"]
+    name = list(filter(lambda x: x["view"] == view_name, app.config.get("REPORTS")))[0]["name"]
 
     return render_template(
         "report.html",
